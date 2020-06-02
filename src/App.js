a6db20867436bf70cf35e315306f0f56724f3e9d
zaidahmed332675 <56406369+zaidahmed332675@users.noreply.github.com>
2020-06-03 00:50:03 +0500

    Automate Surge Desployment

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <br/>
+          By Zaid Ahmed
         </p>
         <a
           className="App-link"
